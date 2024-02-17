40f317f08fc29d26eee8ed6745d5a0e7ca1bb1cc
정목 <126846468+M-ung@users.noreply.github.com>
2024-02-17 21:13:11 +0900

    [Silver IV] Title: 카드2, Time: 200 ms, Memory: 45596 KB -BaekjoonHub

--- 백준/Silver/2164. 카드2/카드2.java
+++ 백준/Silver/2164. 카드2/카드2.java
@@ -1,22 +1,35 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.LinkedList;
 import java.util.Queue;
-import java.util.Scanner;
+import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        Queue<Integer> queue = new LinkedList<Integer>();
-        int n = sc.nextInt();
-        for(int i=1; i<=n; i++)
-        {
-            queue.offer(i);
+    static Queue<Integer> queue;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        queue = new LinkedList<>();
+
+        for(int i=1; i<=N; i++) {
+            queue.add(i);
         }
 
         while(queue.size() > 1) {
-            queue.poll();
-            int temp = queue.poll();
-            queue.offer(temp);
+            poll(queue);
+            back(queue);
         }
         System.out.println(queue.poll());
+    }
+    private static Queue<Integer> poll(Queue<Integer> queue) {
+        queue.poll();
+        return queue;
+    }
+    private static Queue<Integer> back(Queue<Integer> queue) {
+        queue.add(queue.poll());
+        return queue;
     }
 }
