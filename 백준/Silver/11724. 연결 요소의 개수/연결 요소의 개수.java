e811aadf2f60b92bcca2c50796dd876b3ebefc3a
정목 <126846468+M-ung@users.noreply.github.com>
2024-02-24 21:30:16 +0900

    [Silver II] Title: 연결 요소의 개수, Time: 688 ms, Memory: 141556 KB -BaekjoonHub

--- 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.java
+++ 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.java
@@ -1,44 +1,51 @@
-import java.io.IOException;
-import java.util.Scanner;
+import java.io.*;
+import java.util.ArrayList;
+import java.util.StringTokenizer;
 
 public class Main {
-    static int N;
-    static int M;
-    static int[][] arr; // 정점과 간선의 관계를 나타낼 2차원 배열
-    static boolean[] check; // 이미 호출된 정점 체크
-    static int count;
-
+    static ArrayList<Integer>[] arr;
+    static boolean visited[];
     public static void main(String[] args) throws IOException {
-        Scanner sc = new Scanner(System.in);
-        N = sc.nextInt();
-        M = sc.nextInt();
-        arr = new int[N+1][N+1]; // 정점과 간선의 관계를 나타낼 2차원 배열
-        check = new boolean[N+1]; // 이미 호출된 정점 체크
-        count = 0;
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        for(int i=1; i<=M; i++)
-        {
-            int u = sc.nextInt();
-            int v = sc.nextInt();
-            arr[u][v] = 1;
-            arr[v][u] = 1;
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        arr = new ArrayList[N+1];
+        visited = new boolean[N+1];
+
+        for(int i=1; i<N+1; i++) {
+            arr[i] = new ArrayList<>();
         }
-        for(int i=1; i<=N; i++)
-        {
-            if(!check[i]) // V 정점과 연결되어 있으며, 호출이 되지 않은 정점일 경우
-            {
+        for(int i=0; i<M; i++) {
+            st = new StringTokenizer(br.readLine());
+            int s = Integer.parseInt(st.nextToken());
+            int e = Integer.parseInt(st.nextToken());
+            arr[s].add(e);
+            arr[e].add(s);
+        }
+
+        int count = 0;
+        for(int i=1; i<N+1; i++) {
+            if(!visited[i]) {
                 count++;
-                dfs(i); // dfs 호출(재귀함수)
+                DFS(i);
             }
         }
-        System.out.println(count);
-        sc.close();
+        bw.write(count+"");
+        bw.flush();
+        bw.close();
     }
-    public static void dfs(int V) { // dfs 함수
-        check[V] = true;
-        for(int i = 1; i <= N; i++) {
-            if(check[i] == false && arr[V][i] == 1) {
-                dfs(i);
+    static void DFS(int v) {
+        if(visited[v]) {
+            return;
+        }
+        visited[v] = true;
+        for(int i : arr[v]) {
+            if(!visited[i]) {
+                DFS(i);
             }
         }
     }
