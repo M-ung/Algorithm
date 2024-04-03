153fd9a42bcbbfb9fd0d42bdb08bc1f1153a6573
정목 <126846468+M-ung@users.noreply.github.com>
2024-04-03 22:09:55 +0900

    [Silver III] Title: 소수 구하기, Time: 868 ms, Memory: 35516 KB -BaekjoonHub

--- 백준/Silver/1929. 소수 구하기/소수 구하기.java
+++ 백준/Silver/1929. 소수 구하기/소수 구하기.java
@@ -1,12 +1,15 @@
+import java.io.BufferedReader;
 import java.io.IOException;
-import java.util.Scanner;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        Scanner sc = new Scanner(System.in);
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
-        int M = sc.nextInt();
-        int N = sc.nextInt();
+        int M = Integer.parseInt(st.nextToken());
+        int N = Integer.parseInt(st.nextToken());
 
         int[] A = new int[N+1];
 
@@ -17,7 +20,7 @@ public class Main {
             if(A[i] == 0) {
                 continue;
             }
-            for(int j=i+i; j <= N; j=j+i) {
+            for(int j=i*2; j <= N; j=j+i) {
                 A[j] = 0;
             }
         }
