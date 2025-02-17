1e5dcee647ca221f42315d53d74200647e9c36cd
정목 <126846468+M-ung@users.noreply.github.com>
2025-02-17 23:36:55 +0900

    [level 2] Title: 메뉴 리뉴얼, Time: 26.40 ms, Memory: 99.2 MB -BaekjoonHub

--- 프로그래머스/2/72411. 메뉴 리뉴얼/메뉴 리뉴얼.java
+++ 프로그래머스/2/72411. 메뉴 리뉴얼/메뉴 리뉴얼.java
@@ -3,40 +3,33 @@ import java.util.*;
 class Solution {
     public String[] solution(String[] orders, int[] course) {
         List<String> result = new ArrayList<>();
-        
-        for (int size : course) {
-            Map<String, Integer> countMap = new HashMap<>();
-            int maxCount = 0;
-
-            for (String order : orders) {
-                char[] arr = order.toCharArray();
-                Arrays.sort(arr); 
-                findCombinations(new String(arr), size, 0, "", countMap);
+        for(int c : course) {
+            int max = 0;
+            Map<String, Integer> maps = new HashMap<String, Integer>(); 
+            for(String order : orders) {
+                char[] chr = order.toCharArray();
+                Arrays.sort(chr);
+                backTrack(new String(chr), "", c, 0, maps);
             }
-
-            for (int count : countMap.values()) {
-                maxCount = Math.max(maxCount, count);
+            for(int count : maps.values()) {
+                max = Math.max(max, count);
             }
-
-            for (Map.Entry<String, Integer> entry : countMap.entrySet()) {
-                if (entry.getValue() == maxCount && maxCount >= 2) {
+            for (Map.Entry<String, Integer> entry : maps.entrySet()) {
+                if (entry.getValue() == max && max >= 2) {
                     result.add(entry.getKey());
                 }
             }
         }
-
         Collections.sort(result);
         return result.toArray(new String[0]);
     }
-
-    private void findCombinations(String order, int size, int index, String current, Map<String, Integer> countMap) {
-        if (current.length() == size) {
-            countMap.put(current, countMap.getOrDefault(current, 0) + 1);
+    private static void backTrack(String order, String current, int course, int index, Map<String, Integer> maps) {
+        if(current.length() == course) {
+            maps.put(current, maps.getOrDefault(current, 0) + 1);
             return;
         }
-
-        for (int i = index; i < order.length(); i++) {
-            findCombinations(order, size, i + 1, current + order.charAt(i), countMap);
+        for(int i=index; i<order.length(); i++) {
+            backTrack(order, current + order.charAt(i), course, i+1, maps);
         }
     }
 }
