8beb2db69152a5e72f231f25f5b0ececeb2f4596
_.mung <wjdahrrla@gmail.com>
2025-07-15 13:47:25 +0900

    [Gold IV] Title: 고층 건물, Time: 104 ms, Memory: 14224 KB -BaekjoonHub

--- 백준/Gold/1027. 고층 건물/고층 건물.java
+++ 백준/Gold/1027. 고층 건물/고층 건물.java
@@ -4,6 +4,7 @@ import java.util.StringTokenizer;
 
 public class Main {
     static int N;
+    static int visible;
     static int[] heights;
 
     public static void main(String[] args) throws Exception {
@@ -19,45 +20,31 @@ public class Main {
         int answer = 0;
 
         for (int i = 0; i < N; i++) {
-            int visible = 0;
-
-            // 왼쪽
+            visible = 0;
             for (int j = i - 1; j >= 0; j--) {
-                boolean canSee = true;
-                for (int k = j + 1; k < i; k++) {
-                    // j ~ i 직선에 대해, 중간 건물 k가 위에 있는지 판단
-                    long dy1 = heights[i] - heights[j];
-                    long dx1 = i - j;
-                    long dy2 = heights[k] - heights[j];
-                    long dx2 = k - j;
-                    // dy2/dx2 >= dy1/dx1 이면, 시야 차단
-                    if (dy2 * dx1 >= dy1 * dx2) {
-                        canSee = false;
-                        break;
-                    }
-                }
-                if (canSee) visible++;
+                visible = checkCanSee(i, j);
             }
-
-            // 오른쪽
             for (int j = i + 1; j < N; j++) {
-                boolean canSee = true;
-                for (int k = i + 1; k < j; k++) {
-                    long dy1 = heights[j] - heights[i];
-                    long dx1 = j - i;
-                    long dy2 = heights[k] - heights[i];
-                    long dx2 = k - i;
-                    if (dy2 * dx1 >= dy1 * dx2) {
-                        canSee = false;
-                        break;
-                    }
-                }
-                if (canSee) visible++;
+                visible = checkCanSee(j, i);
             }
-
             answer = Math.max(answer, visible);
         }
-
         System.out.println(answer);
     }
-} 
+
+    private static int checkCanSee(int i, int j) {
+        boolean canSee = true;
+        for (int k = j + 1; k < i; k++) {
+            long dy1 = heights[i] - heights[j];
+            long dx1 = i - j;
+            long dy2 = heights[k] - heights[j];
+            long dx2 = k - j;
+            if (dy2 * dx1 >= dy1 * dx2) {
+                canSee = false;
+                break;
+            }
+        }
+        if (canSee) visible++;
+        return visible;
+    }
+}
